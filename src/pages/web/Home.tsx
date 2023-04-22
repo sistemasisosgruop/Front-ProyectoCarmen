3b73dff1ceaccac098c08115055aaf1fd90f60b6
yoelvp <yoelvp73@gmail.com>
2023-04-21 23:50:44 -0500

    agregue el componente de busqueda de servicios

--- src/pages/web/Home.tsx
+++ src/pages/web/Home.tsx
@@ -1,12 +1,19 @@
-import Layout from '../../components/Layout'
+import LayoutPage from '../../components/layouts/LayoutPage'
+import Section from '../../components/layouts/Section'
+import Masthead from '../../components/Masthead'
+import ServiceSearch from '../../components/home/ServiceSearch'
 
 const Home = (): JSX.Element => {
   return (
-    <Layout title='Carmen Agencia de Viajes y Turismo'>
-      <h1 className='text-4xl text-center uppercase'>
-        Homepage
-      </h1>
-    </Layout>
+    <LayoutPage title='Carmen Agencia de Viajes y Turismo'>
+      <Masthead />
+      <ServiceSearch />
+      <Section>
+        <h2 className='text-2xl font-bold h-96'>
+          Habitaciones populares
+        </h2>
+      </Section>
+    </LayoutPage>
   )
 }
 
