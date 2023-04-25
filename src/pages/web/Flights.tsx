16dcc9e9bcdf86f2f74b0212efb3b6896ab76c6f
yoelvp <yoelvp73@gmail.com>
2023-04-24 23:05:46 -0500

    agregue todas las paginas

---
+++ src/pages/web/Flights.tsx
@@ -0,0 +1,14 @@
+import LayoutPage from '../../components/layouts/LayoutPage'
+
+const Flights = (): JSX.Element => {
+  return (
+    <LayoutPage title='Vuelos - Carmen Agencia de Viajes y Turismo'>
+      <h1>
+        Vuelos
+      </h1>
+    </LayoutPage>
+  )
+}
+
+export default Flights
+
---
+++ src/pages/web/Packages.tsx
@@ -0,0 +1,14 @@
+import LayoutPage from '../../components/layouts/LayoutPage'
+
+const Packages = (): JSX.Element => {
+  return (
+    <LayoutPage title='Paquetes turisticos - Carmen Agencia de Viajes y Turismo'>
+      <h1>
+        Paquetes
+      </h1>
+    </LayoutPage>
+  )
+}
+
+export default Packages
+
