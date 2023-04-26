985db758c2f1391faccaf9cee2db4beb2a7588d4
yoelvp <yoelvp73@gmail.com>
2023-04-26 14:12:25 -0500

    agregue un custom hook para el scroll

---
+++ src/hooks/useScrollPageY.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+interface FunctionTypes {
+  isScrolled: boolean
+}
+
+export const useScrollPageY = (): FunctionTypes => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.pageYOffset > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return { isScrolled }
+}
