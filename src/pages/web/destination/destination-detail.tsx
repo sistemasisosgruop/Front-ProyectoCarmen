4531a63019cd14e426c99a7429531b2e7ca67d45
yoelvp <yoelvp73@gmail.com>
2023-05-04 16:38:10 -0500

    pagina de detalle de los destinos finalizada v0.01

--- src/pages/web/destination/destination-detail.tsx
+++ src/pages/web/destination/destination-detail.tsx
@@ -1,15 +1,20 @@
 import LayoutPage from '../../../components/layouts/layout-page'
 import Section from '../../../components/layouts/section'
-import SliderDetail from '../../../components/destinations-catalog/slider-detail'
-import DestinationForm from '../../../components/destinations-catalog/destination-form'
+import SliderDetail from '../../../components/destinations/slider-detail'
+import DestinationForm from '../../../components/destinations/destination-form'
+import TourDetails from '../../../components/destinations/tour-details'
+import DestinationReview from '../../../components/destinations/destination-review'
+import Comments from '../../../components/destinations/comments'
+import Related from '../../../components/destinations/related'
 import { AiFillDashboard, AiFillStar } from 'react-icons/ai'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import { HiOutlineIdentification, HiUsers, HiOutlineLanguage } from 'react-icons/hi2'
 import { CgSandClock } from 'react-icons/cg'
+import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from 'react-icons/hi'
 
 const DestinationDetail = (): JSX.Element => {
   return (
-    <LayoutPage title='Detalle'>
+    <LayoutPage title='Detalles de destino populares'>
       <SliderDetail />
 
       <Section className='my-4 mb-32'>
@@ -137,10 +142,57 @@ const DestinationDetail = (): JSX.Element => {
         <p className='text-center text-2xl text-dark'>
           Informaci&oacute;n importante a tener en cuenta
         </p>
+
         <hr className='w-96 border-b-2 border-b-gray-600 mx-auto' />
 
-        <div className=''>
+        <div className='my-16'>
+          <TourDetails />
+        </div>
+      </Section>
 
+      <Section className='my-16'>
+        <div className='grid grid-cols-3 gap-16 p-8 rounded-xl bg-white'>
+          <div className='col-span-1'>
+            <DestinationReview />
+          </div>
+          <div className='col-span-2'>
+            <Comments />
+          </div>
+        </div>
+      </Section>
+      <Section className='mx-16'>
+        <hr className='border-b border-b-gray-600' />
+      </Section>
+      <Section className='mt-16 mb-32'>
+        <div className='flex justify-between items-center mb-4'>
+          <div className='mb-8'>
+            <h2 className='text-4xl text-dark font-bold'>
+              Lo mas relacionado
+            </h2>
+            <p className='text-dark text-xl'>
+              No te pierdas nuestras promociones por esta temporada
+            </p>
+          </div>
+          <div className='flex justify-center items-center gap-4'>
+            <HiOutlineArrowSmLeft size={24} className='w-10 h-10 rounded-full bg-transparent border border-gray-600 text-dark flex justify-center items-center' />
+            <HiOutlineArrowSmRight size={24} className='w-10 h-10 rounded-full bg-turquoise border border-turquoise text-white flex justify-center items-center' />
+          </div>
+        </div>
+        <div className='w-full grid grid-cols-3 gap-4'>
+          {[...Array(3)].map((_, index) => (
+            <Related key={index} index={index} />
+          ))}
+        </div>
+        <div className='flex justify-center items-center mt-8'>
+          <button
+            type='button'
+            className='bg-blue rounded-xl px-6 py-1 text-white font-bold flex justify-center items-center gap-2 hover:bg-opacity-80'
+          >
+            <span>
+              Ver mas
+            </span>
+            <HiOutlineArrowSmRight size={20} />
+          </button>
         </div>
       </Section>
     </LayoutPage>
