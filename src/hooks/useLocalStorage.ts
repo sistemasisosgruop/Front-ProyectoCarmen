f7caf0c915668f3e2ae613ab8669a33b57337455
yoelvp <yoelvp73@gmail.com>
2023-05-08 10:59:08 -0500

    refactorizacion de hooks

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 
-export const useLocalStorage = (key: string, initialValue: string): void => {
-  const [storedValue, setStoredValue] = useState(() => {
+interface HookData extends Array<string | ((value: string) => void)> {
+  0: string
+  1: (value: string) => void
+}
+
+export const useLocalStorage = (key: string, initialValue: string): HookData => {
+  const [valueSave, setValueSave] = useState<string>(() => {
     try {
-      const item: string = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
+      const item: string | null = window.localStorage.getItem(key)
+      return item !== null ? JSON.parse(item) : initialValue
     } catch (error) {
       return initialValue
     }
   })
 
-  const setValue = (value: string) => {
+  const setValue = (value: string): void => {
     try {
-      setStoredValue(value)
-      window.localStorage.setItem(key, JSON.stringify(value))
+      setValueSave(value)
     } catch (error) {
       console.log(error)
     }
   }
 
-  return { storedValue, setValue }
+  return [valueSave, setValue]
 }
-
