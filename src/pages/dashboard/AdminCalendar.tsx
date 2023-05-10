6f41bc4f9dfca1e7a1fb9289710354ae42fc7e22
yoelvp <yoelvp73@gmail.com>
2023-05-10 18:30:57 -0500

    modify: pagina de calendario finalizada

--- src/pages/dashboard/AdminCalendar.tsx
+++ src/pages/dashboard/AdminCalendar.tsx
@@ -1,14 +1,38 @@
-import Calendar from '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/react'
 import interactionPlugin from '@fullcalendar/interaction'
-import datGridPlugin from '@fullcalendar/daygrid'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import localeEs from '@fullcalendar/core/locales/es'
 import AdminLayout from '../../components/layouts/AdminLayout'
+import AdminSection from '../../components/layouts/AdminSection'
+import HeadingText from '../../components/HeadingText'
 
 const AdminCalendar = (): JSX.Element => {
   return (
     <AdminLayout title='Mi calendario'>
-      <h1>
-        Calendar
-      </h1>
+      <HeadingText text='Mi calendario' />
+
+      <AdminSection>
+        <FullCalendar
+          plugins={[interactionPlugin, dayGridPlugin, timeGridPlugin]}
+          initialView='dayGridMonth'
+          weekends={true}
+          locale={localeEs}
+          themeSystem='standart'
+          headerToolbar={{
+            start: 'prev,next today',
+            center: 'title',
+            end: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          events={[
+            { title: 'Cumpleaños', date: '2023-05-08', color: '#fe3131' },
+            { title: 'Sorpresa a la sopresa', date: '2023-05-08', color: '#fe3131' },
+            { title: 'Dia de la madre', date: '2023-05-01' },
+            { title: 'Prueba', date: '2023-05-03' },
+            { title: 'Dia especial', date: '2023-05-02' }
+          ]}
+        />
+      </AdminSection>
     </AdminLayout>
   )
 }
