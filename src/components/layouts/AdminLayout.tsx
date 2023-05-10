86adc981ce2247b508a38c12edc54d8f79909f90
yoelvp <yoelvp73@gmail.com>
2023-05-10 18:29:39 -0500

    modify: cambio de clases de tailwindcss

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useToggleSidebar } from '../../hooks/useToggleSidebar'
+import { motion } from 'framer-motion'
 import Header from '../sidebar/Header'
 import Sidebar from '../sidebar/Sidebar'
 
@@ -16,21 +17,28 @@ const AdminLayout = ({ children, title }: Props): JSX.Element => {
   }, [])
 
   return (
-    <div className='h-full xl:flex'>
+    <div className='h-full max-w-full xl:flex xl:justify-start xl:items-start'>
       {isShowSidebar && (
-        <Sidebar
-          closeSidebar={closeSidebar}
-        />
+        <motion.div
+          initial={{ height: 0, x: -120, y: -150, opacity: 0 }}
+          animate={{ height: 'auto', x: 0, y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Sidebar
+            closeSidebar={closeSidebar}
+          />
+        </motion.div>
       )}
-      <div className='hidden xl:inline-block'>
+      <div className='hidden xl:inline-block xl:h-screen'>
         <Sidebar />
       </div>
 
-      <div className='w-full h-full bg-gray-300'>
+      <div className='max-w-full h-full bg-gray-300'>
         <Header
           openSidebar={openSidebar}
         />
-        <div className='h-full overflow-y-scroll'>
+
+        <div className='max-w-full overflow-y-scroll p-4' style={{ height: 'calc(100vh - 50px)', width: 'calc(100vw - 320px)' }}>
           {children}
         </div>
       </div>
