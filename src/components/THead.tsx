0eefe22e5343804cfda0a7fa898e20257690557c
yoelvp <yoelvp73@gmail.com>
2023-05-10 18:33:39 -0500

    modify: cambio de prop para los estilos

--- src/components/THead.tsx
+++ src/components/THead.tsx
@@ -1,14 +1,18 @@
 interface Props {
   headers: string[]
+  rowCellStyles?: string
 }
-const THead = ({ headers }: Props): JSX.Element => {
+const THead = ({
+  headers,
+  rowCellStyles = 'bg-gray-50 px-8 py-2 border-b border-b-gray-200 text-start text-dark text-lg'
+}: Props): JSX.Element => {
   return (
     <thead>
       <tr>
         {headers.map((header, index) => (
           <th
             key={index}
-            className='bg-gray-50 px-8 py-2 border-b border-b-gray-200 text-start text-dark text-lg'
+            className={rowCellStyles}
           >
             {header}
           </th>
