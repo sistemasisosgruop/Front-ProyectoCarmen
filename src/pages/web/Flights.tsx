a17d36606e8932ed62ee65f12ac27175fea6072b
yoelvp <yoelvp73@gmail.com>
2023-05-10 14:17:16 -0500

    modify: cambio de estilos

--- src/pages/web/Flights.tsx
+++ src/pages/web/Flights.tsx
@@ -2,34 +2,44 @@ import ContactForm from '../../components/flights/ContactForm'
 import LayoutPage from '../../components/layouts/LayoutPage'
 import Section from '../../components/layouts/Section'
 import Masthead from '../../components/Masthead'
-import Tabs from '../../components/flights/Tabs'
-import TabItem from '../../components/flights/TabItem'
+import Tabs from '../../components/Tabs'
+import TabItem from '../../components/TabItem'
+import PriceList from '../../components/flights/PriceList'
+import SelectFlight from '../../components/flights/SelectFlight'
 
 const Flights = (): JSX.Element => {
   return (
     <LayoutPage title='Vuelos - Carmen Agencia de Viajes y Turismo'>
       <Masthead />
-      <Section>
-        <Tabs>
-          <TabItem number='1' label='Selecciona tu vuelo'>
-            <h2>Buenas 1</h2>
+      <Section className='pt-32 mb-16'>
+        <Tabs
+          tabContainerStyles='flex justify-center items-center'
+          tabStyle='px-16 py-4 text-xl font-bold'
+          contentContainerStyles='bg-transparent mt-8 p-4'
+        >
+          <TabItem label='Selecciona tu vuelo' text='1'>
+            <div className='w-full flex flex-col justify-center items-center gap-16'>
+              <div className='mx-44'>
+                <PriceList />
+              </div>
+              <SelectFlight />
+            </div>
           </TabItem>
-          <TabItem number='2' label='Completa tus datos'>
-            <h2>Buenas 2</h2>
+          <TabItem label='Completa tus datos' text='2'>
+            <h1>
+              Completa tus datos
+            </h1>
           </TabItem>
-          <TabItem number='3' label='Finaliza la reserva'>
-            <h2>
-              Buenas 3
-            </h2>
+          <TabItem label='Finalizar la reserva' text='2'>
+            <h1>
+              Finalizar la reserva
+            </h1>
           </TabItem>
         </Tabs>
-        <h1>
-          Vuelos
-        </h1>
-      </Section>
+      </Section >
 
       {/* Contact form */}
-      <Section className='mb-32'>
+      < Section className='mb-32' >
         <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
           <ContactForm />
 
@@ -39,8 +49,8 @@ const Flights = (): JSX.Element => {
             className='h-full w-full rounded-md border border-white'
           />
         </div>
-      </Section>
-    </LayoutPage>
+      </Section >
+    </LayoutPage >
   )
 }
 
