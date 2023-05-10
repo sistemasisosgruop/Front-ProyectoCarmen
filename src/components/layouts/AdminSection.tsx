65eb016ad5d7ba5e44a3c822d0e28cbb28515e2a
yoelvp <yoelvp73@gmail.com>
2023-05-10 18:30:18 -0500

    add: seccion para las secciones de las paginas

---
+++ src/components/layouts/AdminSection.tsx
@@ -0,0 +1,20 @@
+import { motion } from 'framer-motion'
+
+interface Props {
+  children: JSX.Element | JSX.Element[]
+}
+
+const AdminSection = ({ children }: Props): JSX.Element => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 0, y: 25 }}
+      animate={{ opacity: 1, x: 0, y: 0 }}
+      transition={{ duration: 0.7, type: 'easeInOut' }}
+      className='max-w-full bg-white p-8 rounded-xl'
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default AdminSection
