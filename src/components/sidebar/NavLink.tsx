dcb6f70360b0de7b2d7b79f278f1516e86c6c4ea
yoelvp <yoelvp73@gmail.com>
2023-05-11 11:32:17 -0500

    modify: fondo del link cuando estamos en una seccion

--- src/components/sidebar/NavLink.tsx
+++ src/components/sidebar/NavLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 interface Props {
   href: string
@@ -7,10 +7,13 @@ interface Props {
 }
 
 const NavLink = ({ href, label, icon }: Props): JSX.Element => {
+  const { pathname } = useLocation()
+  console.log(pathname)
+
   return (
     <Link
       to={href}
-      className='w-full text-dark font-bold px-4 py-2 rounded-xl flex justify-start items-center gap-2 hover:bg-orange hover:text-white transition-colors duration-300'
+      className={`w-full text-dark font-bold px-4 py-3 rounded-xl flex justify-start items-center gap-2 ${href === pathname ? 'bg-orange text-white' : ''} hover:bg-orange hover:text-white transition-colors duration-300`}
     >
       {icon}
       <span>
