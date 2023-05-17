6ee21a1958d9c721d3ab4a3d11ec42e5b46e4992
yoelvp <yoelvp73@gmail.com>
2023-05-17 12:18:30 -0500

    modify: cambio de clases para el comp

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -2,14 +2,26 @@ import { GrSubtractCircle } from 'react-icons/gr'
 import { IoMdAddCircleOutline } from 'react-icons/io'
 
 interface Props {
-
   value: number
   onChangeValue: (newValue: number) => void
   label: string
   htmlFor: string
+  showIcon?: boolean
+  icon?: JSX.Element
+  iconPosition?: string
+  textStyle?: string
 }
 
-const NumberPicker = ({ value, onChangeValue, label, htmlFor }: Props): JSX.Element => {
+const NumberPicker = ({
+  value,
+  onChangeValue,
+  label,
+  htmlFor,
+  showIcon,
+  icon,
+  iconPosition,
+  textStyle
+}: Props): JSX.Element => {
   const increaseValue = (): void => {
     onChangeValue(value + 1)
   }
@@ -22,11 +34,17 @@ const NumberPicker = ({ value, onChangeValue, label, htmlFor }: Props): JSX.Elem
     <div className='w-full flex flex-col justify-start items-start'>
       <label
         htmlFor={htmlFor}
-        className='text-gray-600 text-sm mb-1'
+        className={`text-gray-600 mb-1 text-sm flex justify-start items-center gap-2`}
       >
-        {label}
+        <>
+          {showIcon !== undefined && icon}
+          <span className={textStyle}>
+            {label}
+          </span>
+          {showIcon !== undefined && iconPosition === 'right' && icon}
+        </>
       </label>
-      <div className='w-full flex justify-center items-center gap-2'>
+      <div className='w-full flex justify-start items-start gap-2'>
         {value > 1 && (
           <button
             type='button'
