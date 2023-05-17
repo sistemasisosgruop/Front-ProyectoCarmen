527fbd6612f04597c3bb7e762f13daac590d3b60
yoelvp <yoelvp73@gmail.com>
2023-05-17 12:16:19 -0500

    add: componente reutilizables

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,7 @@ interface Props {
   isIcon?: boolean
   icon?: JSX.Element
   position?: string
+  extraClasses?: string
   onClick?: () => void
 }
 
@@ -15,13 +16,14 @@ const Button = ({
   isIcon,
   icon,
   position = 'right',
+  extraClasses,
   onClick
 }: Props): JSX.Element => {
   return (
     <button
       type='button'
       onClick={onClick}
-      className={`w-full ${background} ${textColor} px-6 py-2 rounded-xl text-center font-bold flex justify-center items-center gap-2 hover:bg-opacity-90 sm:w-auto`}
+      className={`w-full ${background} ${textColor} px-6 py-2 rounded-xl text-center font-bold flex justify-center items-center gap-2 hover:bg-opacity-90 sm:w-auto ${extraClasses ?? ''}`}
     >
       {isIcon !== undefined && position === 'left' && icon}
       <span>
