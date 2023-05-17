27394f7f9cc1914225264939067580953cc5a7de
yoelvp <yoelvp73@gmail.com>
2023-05-17 12:20:59 -0500

    modify: agregue modo responsive en toda la pagina

--- src/pages/web/Home.tsx
+++ src/pages/web/Home.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { useLang } from '../../hooks/useLang'
 import LayoutPage from '../../components/layouts/LayoutPage'
 import Section from '../../components/layouts/Section'
@@ -6,9 +5,10 @@ import Heading from '../../components/Heading'
 import Masthead from '../../components/Masthead'
 import ServiceSearch from '../../components/home/ServiceSearch'
 import SliderTourPackage from '../../components/home/SliderTourPackage'
+import Comments from '../../components/home/Comments'
 import Card from '../../components/Card'
-import CommentCard from '../../components/CommentCard'
-import { AiFillStar } from 'react-icons/ai'
+import ButtonLink from '../../components/ButtonLink'
+import { HiOutlineArrowSmRight } from 'react-icons/hi'
 
 const Home = (): JSX.Element => {
   const { t } = useLang()
@@ -46,9 +46,13 @@ const Home = (): JSX.Element => {
           />
         </section>
         <div className='flex justify-center items-center'>
-          <Link to='/catalogo-habitaciones' className='bg-orange text-white font-bold rounded-md px-5 py-2 hover:bg-opacity-90'>
-            <span>Ver catálogo</span>
-          </Link>
+          <ButtonLink
+            to='/catalogo-habitaciones'
+            text='Ver catálogo'
+            showIcon={true}
+            icon={<HiOutlineArrowSmRight size={18} />}
+            iconPosition='right'
+          />
         </div>
       </Section>
 
@@ -76,12 +80,13 @@ const Home = (): JSX.Element => {
           />
         </section>
         <div className='flex justify-center items-center'>
-          <Link
+          <ButtonLink
             to='/destinos-populares'
-            className='bg-orange text-white font-bold rounded-md px-5 py-2 hover:bg-opacity-90'
-          >
-            <span>Ver catálogo</span>
-          </Link>
+            text='Ver catálogo'
+            showIcon={true}
+            icon={<HiOutlineArrowSmRight size={18} />}
+            iconPosition='right'
+          />
         </div>
       </Section>
 
@@ -109,51 +114,18 @@ const Home = (): JSX.Element => {
           />
         </section>
         <div className='flex justify-center items-center'>
-          <button className='bg-orange text-white font-bold rounded-md px-5 py-2 hover:bg-opacity-90'>
-            <span>Ver catálogo</span>
-          </button>
+          <ButtonLink
+            to='/catalogo-habitaciones'
+            text='Ver catálogo'
+            showIcon={true}
+            icon={<HiOutlineArrowSmRight size={18} />}
+            iconPosition='right'
+          />
         </div>
       </Section>
 
       <Section className='bg-white pt-32'>
-        <div className='xl:px-32'>
-          <h2 className='text-2xl tetx-dark font-bold text-center xl:text-4xl'>
-            {t('home.commentsSection.theCommentOf')} <span className='text-orange'>{t('home.commentsSection.ourClients')}</span>
-          </h2>
-          <p className='text-xl text-dark text-center xl:text-2xl'>
-            {t('home.commentsSection.description')}
-          </p>
-          <hr className='border-b-2 border-b-gray-400 mt-4' />
-        </div>
-        <div className='flex justify-center items-center gap-8 py-8 xl:gap-32'>
-          <p className='flex flex-col justify-center items-center gap-1 text-center'>
-            <span className='text-2xl text-dark font-bold xl:text-6xl'>
-              5.0
-            </span>
-            <span className='text-sm md:text-base'>
-              {t('home.commentsSection.qualification')}
-            </span>
-          </p>
-          <div className='text-yellow-600 flex justify-start gap-2'>
-            <AiFillStar size={32} />
-            <AiFillStar size={32} />
-            <AiFillStar size={32} />
-            <AiFillStar size={32} />
-            <AiFillStar size={32} />
-          </div>
-        </div>
-        <div className='flex flex-col gap-8 pb-16 md:px-16'>
-          <CommentCard
-            username='Maria Luisa Paredes Diaz'
-            comment='scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate.'
-            qualification={5}
-          />
-          <CommentCard
-            username='Edin Yoel Valverde Polo'
-            comment='scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate.'
-            qualification={5}
-          />
-        </div>
+        <Comments />
       </Section>
     </LayoutPage >
   )
