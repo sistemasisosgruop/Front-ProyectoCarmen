aa0260ed1a07b27b07e079fb956c439a754934b0
yoelvp <yoelvp73@gmail.com>
2023-05-18 23:33:18 -0500

    delete: animacion a las secciones

--- src/components/layouts/Section.tsx
+++ src/components/layouts/Section.tsx
@@ -1,6 +1,3 @@
-import { motion } from 'framer-motion'
-import { useRef } from 'react'
-
 interface Props {
   children: JSX.Element[] | JSX.Element
   className?: string
@@ -10,19 +7,10 @@ const Section = ({
   children,
   className = ''
 }: Props): JSX.Element => {
-  const sectionRef = useRef<HTMLDivElement>(null)
-
   return (
-    <motion.div
-      ref={sectionRef}
-      initial={{ opacity: 0, x: 0, y: 50 }}
-      animate={{ opacity: 1, x: 0, y: 0 }}
-      exit={{ opacity: 0, x: -0, y: 50 }}
-      transition={{ duration: 0.9, type: 'easeInOut' }}
-      className={`px-8 sm:px-16 md:px-24 xl:px-32 ${className}`}
-    >
+    <div className={`px-8 sm:px-16 md:px-24 xl:px-32 ${className}`}>
       {children}
-    </motion.div>
+    </div>
   )
 }
 
