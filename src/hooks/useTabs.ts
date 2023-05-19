230355e9d06e517c0c6ec57a3e32d82f434d4bce
yoelvp <yoelvp73@gmail.com>
2023-05-18 23:12:23 -0500

    add: hook para administrar los tabs

---
+++ src/hooks/useTabs.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+interface TabsState {
+  activeTab: number
+  switchTab: (tabIndex: number) => void
+}
+
+export const useTabs = (initialTab: number = 0): TabsState => {
+  const [activeTab, setActiveTab] = useState<number>(initialTab)
+
+  const switchTab = (tabIndex: number): void => {
+    setActiveTab(tabIndex)
+  }
+
+  return { activeTab, switchTab }
+}
