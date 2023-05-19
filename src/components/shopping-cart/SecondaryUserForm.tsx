b3f083377dcbc8fafefb363c264cb8cb83c3456a
yoelvp <yoelvp73@gmail.com>
2023-05-18 22:54:55 -0500

    add: card del segundo usuario (no definido)

---
+++ src/components/shopping-cart/SecondaryUserForm.tsx
@@ -0,0 +1,21 @@
+import InformationForm from './InformationForm'
+
+const SecondaryUserForm = (): JSX.Element => {
+  return (
+    <div>
+      <h2 className='flex justify-start items-center gap-2 font-bold mb-4'>
+        <span className='w-8 h-8 rounded-full bg-orange text-white text-xl flex justify-center items-center'>
+          2
+        </span>
+        <span className='text-xl text-dark'>
+          Datos del segundo usuario
+        </span>
+      </h2>
+      <form className='flex flex-col gap-4'>
+        <InformationForm />
+      </form>
+    </div>
+  )
+}
+
+export default SecondaryUserForm
