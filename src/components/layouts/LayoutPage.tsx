90166482acbfb7bb373e8fcf6f5975bf2edba9ac
yoelvp <yoelvp73@gmail.com>
2023-05-18 23:33:34 -0500

    add: animacion a las pagina esceptuando el header

--- src/components/layouts/LayoutPage.tsx
+++ src/components/layouts/LayoutPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import Footer from '../footer/Footer'
 import Header from '../header/Header'
 
@@ -19,9 +20,17 @@ const LayoutPage = ({ children, title }: Props): JSX.Element => {
   return (
     <>
       <Header />
-      <div className='mt-[98px] xl:mt-[154px]'>
-        {children}
-      </div>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, x: 0, y: -5 }}
+          animate={{ opacity: 1, x: 0, y: 0 }}
+          exit={{ opacity: 0, x: 0, y: -5 }}
+          transition={{ duration: 0.7, type: 'ease' }}
+          className='mt-[98px] xl:mt-[154px]'
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
       <Footer />
     </>
   )
