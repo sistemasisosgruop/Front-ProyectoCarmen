ecf6d02465e7645d1bd3208ffc4068010054fd0c
yoelvp <yoelvp73@gmail.com>
2023-05-20 00:13:59 -0500

    add: traducciones

--- src/components/comments/CommentInput.tsx
+++ src/components/comments/CommentInput.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { useLang } from '@hooks/useLang'
 import { BsStarFill } from 'react-icons/bs'
 import { HiOutlineArrowSmRight } from 'react-icons/hi'
 
 const CommentInput = (): JSX.Element => {
   const [selectedStar, setSelectedStar] = useState<number>(0)
+  const { t } = useLang()
 
   const handleStarClick = (index: number): void => {
     setSelectedStar(index + 1)
@@ -12,10 +14,10 @@ const CommentInput = (): JSX.Element => {
   return (
     <form>
       <h4 className='text-dark text-center text-2xl font-bold'>
-        Agregar una reseña
+        {t('general.add_review')}
       </h4>
       <p className='text-dark text-center'>
-        ¡Agrega un comentario y calificanos!
+        {t('general.add_comment_rate_us')}
       </p>
       <div className='mt-8'>
         <div className='flex justify-center items-center gap-1 mb-4'>
@@ -33,7 +35,7 @@ const CommentInput = (): JSX.Element => {
         <div className='relative'>
           <textarea
             name='comment'
-            placeholder='Agregar un comentario...'
+            placeholder={`${t('general.add_comment')}...`}
             className='w-full border border-gray-400 rounded-xl px-4 pt-2 pb-16 outline-none resize-none focus:border-orange'
             rows={12}
           ></textarea>
@@ -42,7 +44,7 @@ const CommentInput = (): JSX.Element => {
             className='absolute bottom-4 right-4 bg-orange flex justify-center items-center gap-2 text-white font-bold px-6 py-2 rounded-xl'
           >
             <span>
-              Publicar
+              {t('general.comment')}
             </span>
             <HiOutlineArrowSmRight size={20} />
           </button>
