2e716883a4f8096c86b7429857acc18f990e6fee
yoelvp <yoelvp73@gmail.com>
2023-05-22 17:57:38 -0500

    add: props para los estilos y mas customizacion

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,20 +2,24 @@ interface Props {
   title: string
   description?: string | null
   textAlign?: string
+  titleStyles?: string
+  descriptionStyles?: string
 }
 
 const Heading = ({
   title,
   textAlign = 'text-center',
-  description
+  description,
+  titleStyles,
+  descriptionStyles
 }: Props): JSX.Element => {
   return (
-    <div className='mb-8'>
-      <h2 className={`text-4xl ${textAlign ?? ''} font-bold`}>
+    <div className='mb-4'>
+      <h2 className={`text-2xl ${textAlign ?? ''} font-bold ${titleStyles ?? ''} md:text-4xl`}>
         {title}
       </h2>
       {description !== undefined && (
-        <p className={`text-2xl text-gray-800 ${textAlign ?? ''}`}>
+        <p className={`text-xl text-gray-800 ${textAlign ?? ''} ${descriptionStyles ?? ''} xl:text-2xl`}>
           {description}
         </p>
       )}
