a6418bc5f68e42a1a2070d19c1a05d52239e9022
yoelvp <yoelvp73@gmail.com>
2023-05-24 10:27:44 -0500

    modify: agregue el hook tabs y cambio de rutas de los componentes

--- src/pages/web/Flights.tsx
+++ src/pages/web/Flights.tsx
@@ -1,14 +1,16 @@
-import ContactForm from '../../components/flights/ContactForm'
-import LayoutPage from '../../components/layouts/LayoutPage'
-import Section from '../../components/layouts/Section'
-import Masthead from '../../components/Masthead'
-import Tabs from '../../components/Tabs'
-import TabItem from '../../components/TabItem'
-import PriceList from '../../components/flights/PriceList'
-import SelectFlight from '../../components/flights/SelectFlight'
+import { useTabs } from '@hooks/useTabs'
+import LayoutPage from '@layouts/LayoutPage'
+import Section from '@layouts/Section'
+import Masthead from '@components/Masthead'
+import Tabs from '@components/Tabs'
+import TabItem from '@components/TabItem'
+import PriceList from '@components/flights/PriceList'
+import SelectFlight from '@components/flights/SelectFlight'
 import ContactFormSection from '@components/contact-form/ContactFormSection'
 
 const Flights = (): JSX.Element => {
+  const { activeTab, switchTab } = useTabs(0)
+
   return (
     <LayoutPage title='Vuelos - Carmen Agencia de Viajes y Turismo'>
       <Masthead />
@@ -17,6 +19,8 @@ const Flights = (): JSX.Element => {
           tabContainerStyles='flex justify-center items-center'
           tabStyle='px-16 py-4 text-xl font-bold'
           contentContainerStyles='bg-transparent mt-8 p-4'
+          activeTab={activeTab}
+          switchTab={switchTab}
         >
           <TabItem label='Selecciona tu vuelo' text='1'>
             <div className='w-full flex flex-col justify-center items-center gap-16'>
