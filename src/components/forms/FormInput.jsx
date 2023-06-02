1cf69a272888c078db490906580b6bbabfa29564
yoelvp <yoelvp73@gmail.com>
2023-06-02 10:18:58 -0500

    add: proptypes

--- src/components/forms/FormInput.jsx
+++ src/components/forms/FormInput.jsx
@@ -1,34 +1,53 @@
-const MyFormInput = ({
+import Types from 'prop-types'
+
+const FormInput = ({
+  name,
+  label,
+  type = 'text',
+  placeholder,
   register,
   rules,
   errors,
-  name,
-  label,
   labelColor = 'text-gray-600',
   extraClasses
 }) => {
   return (
     <div
-      className={`w-full flex flex-col justify-start items-start ${
-        extraClasses ?? ''
-      }`}
+      className={`w-full flex flex-col justify-start items-start ${extraClasses}`}
     >
-      <label htmlFor={name} className={`${labelColor ?? ''} text-sm mb-1`}>
-        {label}
-      </label>
+      <article className="w-full flex justify-between items-center">
+        <label htmlFor={name} className={`${labelColor} text-sm mb-1`}>
+          {label}
+        </label>
+        {errors[name]?.type === 'required' && (
+          <span className="text-red-400 text-sm">{errors[name]?.message}</span>
+        )}
+        {errors[name]?.type === 'minLength' && (
+          <span className="text-red-400 text-sm">
+            The min lenght is 10 characters
+          </span>
+        )}
+      </article>
       <input
-        type="text"
-        placeholder=""
-        onBlur={() => {
-          register(name, rules)
-        }}
+        type={type}
+        placeholder={placeholder}
+        {...register(name, rules)}
         className="w-full border border-gray-400 text-gray-600 rounded-xl px-4 py-2 focus:outline-none focus:border-blue focus:text-blue"
       />
-      <span>
-        {errors[name]?.type === 'required' && 'El campo es requerido'}
-      </span>
     </div>
   )
 }
 
-export default MyFormInput
+FormInput.propTypes = {
+  label: Types.string.isRequired,
+  name: Types.string.isRequired,
+  type: Types.string,
+  placeholder: Types.string,
+  register: Types.func.isRequired,
+  rules: Types.object.isRequired,
+  errors: Types.object,
+  labelColor: Types.string,
+  extraClasses: Types.string
+}
+
+export default FormInput
