55055420b5e1e53f4b824947b1a52bd0868fe2ab
yoelvp <yoelvp73@gmail.com>
2023-06-07 09:44:46 -0500

    modify: agrega todas las props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,8 @@ const Button = ({
   icon,
   position = 'right',
   extraClasses,
-  onClick
+  onClick,
+  ...props
 }) => {
   return (
     <button
@@ -18,6 +19,7 @@ const Button = ({
       className={`w-full ${background} ${textColor} px-6 py-2 rounded-xl text-center font-bold flex justify-center items-center gap-2 hover:bg-opacity-90 sm:w-auto ${
         extraClasses ?? ''
       }`}
+      {...props}
     >
       {showIcon !== undefined && position === 'left' && icon}
       <span>{text}</span>
