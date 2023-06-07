55fdb47c46acaaa23e40e558205e78d15f2b0e45
yoelvp <yoelvp73@gmail.com>
2023-06-07 09:48:21 -0500

    modify: implementacion de los Toast y eliminacion de .then

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,34 @@ import { AuthProvider } from '@context/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { I18nextProvider, initReactI18next } from 'react-i18next'
 import i18next from 'i18next'
+import { ToastContainer } from 'react-toastify'
 import Routes from './routes/Routes'
 
 import globalEs from './lang/es/global.json'
 import globalEn from './lang/en/global.json'
 
-i18next
-  .use(initReactI18next)
-  .init({
-    interpolation: {
-      escapeValue: false
+import 'react-toastify/dist/ReactToastify.css'
+
+i18next.use(initReactI18next).init({
+  interpolation: {
+    escapeValue: false
+  },
+  lng: localStorage.getItem('lang') ?? 'es',
+  resources: {
+    es: {
+      global: globalEs
     },
-    lng: localStorage.getItem('lang') ?? 'es',
-    resources: {
-      es: {
-        global: globalEs
-      },
-      en: {
-        global: globalEn
-      }
+    en: {
+      global: globalEn
     }
-  })
-  .then(response => {
-    console.log(response)
-  })
-  .catch(error => {
-    console.log(error)
-  })
+  }
+})
 
 const App = () => {
   return (
     <I18nextProvider i18n={i18next}>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Routes />
         </BrowserRouter>
