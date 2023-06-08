856c95702033377154eec31162de02006faa3a11
yoelvp <yoelvp73@gmail.com>
2023-06-08 12:14:33 -0500

    modify: componetización para los cupones

--- src/pages/dashboard/settings/Coupons.jsx
+++ src/pages/dashboard/settings/Coupons.jsx
@@ -3,12 +3,11 @@ import AdminLayout from '@layouts/AdminLayout'
 import AdminSection from '@layouts/AdminSection'
 import Modal from '@components/Modal'
 import Heading from '@components/Heading'
-import THead from '@components/THead'
 import Button from '@components/Button'
-import AddCouponForm from '@components/coupons/AddCouponForm'
+import AddCoupon from '@components/coupons/AddCoupon'
+import CouponList from '@components/coupons/CouponList'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { IoMdAdd } from 'react-icons/io'
-import { BiEdit, BiTrash } from 'react-icons/bi'
 
 const Coupons = () => {
   const { isOpenModal, openModal, closeModal } = useModal()
@@ -39,55 +38,18 @@ const Coupons = () => {
               text="Agregar"
               onClick={openModal}
               background="bg-orange"
-              isIcon={true}
+              showIcon={true}
               icon={<IoMdAdd size={18} />}
             />
           </article>
 
-          <div className="overflow-x-scroll rounded-xl">
-            <table className="w-full">
-              <THead
-                headers={[
-                  '#',
-                  'Código de cipón',
-                  'Descuento',
-                  'Producto asignado',
-                  'Acciones'
-                ]}
-                rowCellStyles="bg-blue text-white px-2 py-2 whitespace-nowrap"
-              />
-              <tbody>
-                {[...Array(5)].map((_, index) => (
-                  <tr key={index} className="text-sm">
-                    <td>
-                      <p className="text-center font-bold">{index + 1}</p>
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-2">AX0192</td>
-                    <td className="px-4 py-2">15 %</td>
-                    <td className="whitespace-nowrap px-4 py-2">
-                      Departamento
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-2">
-                      <article className="flex justify-center items-center gap-2">
-                        <button type="button" className="p-1 text-dark">
-                          <BiEdit size={18} />
-                        </button>
-                        <button type="button" className="p-1 text-dark">
-                          <BiTrash size={18} />
-                        </button>
-                      </article>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <CouponList isOpenModal={isOpenModal} />
         </AdminSection>
       </AdminLayout>
 
       {isOpenModal && (
         <Modal title="Crear cupón" closeModal={closeModal}>
-          <AddCouponForm closeModal={closeModal} />
+          <AddCoupon closeModal={closeModal} />
         </Modal>
       )}
     </>
