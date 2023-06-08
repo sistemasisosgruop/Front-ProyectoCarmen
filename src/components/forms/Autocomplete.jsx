da128cb1f1edc10e753e736670036e02ae874a0a
yoelvp <yoelvp73@gmail.com>
2023-06-08 12:14:08 -0500

    modify: add register form

--- src/components/forms/Autocomplete.jsx
+++ src/components/forms/Autocomplete.jsx
@@ -1,21 +1,22 @@
 import { AiOutlineCaretDown } from 'react-icons/ai'
 
-const Autocomplete = ({ label, results, name }) => {
+const Autocomplete = ({ id, label, results, register }) => {
   return (
     <div>
-      <label htmlFor={name} className="text-gray-600 text-sm inline-block mb-1">
+      <label htmlFor={id} className="text-gray-600 text-sm inline-block mb-1">
         {label}
       </label>
       <article className="relative w-full">
         <input
           type="search"
-          list={name}
+          list={id}
           placeholder={label}
           className="w-full border border-gray-400 rounded-xl px-6 py-2 text-gray-600 focus:outline-none focus:border-blue focus:text-blue"
+          {...register(id)}
         />
         <AiOutlineCaretDown className="absolute top-1/2 right-2 -translate-y-1/2 text-gray-600" />
       </article>
-      <datalist id={name}>
+      <datalist id={id}>
         {results.map((result, index) => (
           <option key={index} value={result.value}>
             {result.text}
