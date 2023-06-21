43933b6552005e86712ec5b0d66e4b77fc507006
yoelvp <yoelvp73@gmail.com>
2023-06-21 18:57:20 -0500

    agregue tipado de las props en el componente

--- src/components/forms/FormTextArea.tsx
+++ src/components/forms/FormTextArea.tsx
@@ -1,29 +1,34 @@
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
+
+interface Props {
+  register: UseFormRegister<FieldValues>
+  errors: FieldErrors
+  required: boolean
+  name: string
+  label?: string
+  labelColor?: string
+  extraClasses?: string
+}
 const FormTextArea = ({
   register,
-  rules,
   errors,
+  required,
   name,
   label,
   labelColor = 'text-gray-600',
   extraClasses
-}) => {
+}: Props) => {
   return (
-    <div
-      className={`flex flex-col justify-start items-start ${
-        extraClasses ?? ''
-      }`}
-    >
+    <div className={`flex flex-col justify-start items-start ${extraClasses ?? ''}`}>
       <label htmlFor={name} className={`${labelColor ?? ''} text-sm mb-1`}>
         {label}
       </label>
       <textarea
-        {...register(name, rules)}
+        {...register(name, { required })}
         rows={5}
         className="w-full border border-gray-400 text-gray-600 rounded-xl px-4 py-2 resize-none focus:outline-none focus:border-blue focus:text-blue"
       ></textarea>
-      <span>
-        {errors[name]?.type === 'required' && 'El campo es requerido'}
-      </span>
+      <span>{errors[name]?.type === 'required' && 'El campo es requerido'}</span>
     </div>
   )
 }
