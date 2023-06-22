92e6f45ce052764da94fbcc8d752c049408589c5
yoelvp <yoelvp73@gmail.com>
2023-06-21 19:18:48 -0500

    tipado de props y variables en lso formularios

--- src/features/tours/components/AddSecondForm.tsx
+++ src/features/tours/components/AddSecondForm.tsx
@@ -1,66 +1,82 @@
 import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import Button from '@components/Button'
-import MyFormInput from '@forms/FormInput'
-import FormTextArea from '@forms/FormTextArea'
+import Input from '@forms/Input'
+import Textarea from '@forms/Textarea'
 import UploadImages from '@components/UploadImages'
 import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from 'react-icons/hi'
+import {
+  Control,
+  FieldErrors,
+  FieldValues,
+  UseFormHandleSubmit
+} from 'react-hook-form'
 
 interface Props {
-  control: any
-  errors: any
-  handleSubmit: any
-  setFiles: () => void
+  control: Control
+  errors: FieldErrors
+  handleSubmit: UseFormHandleSubmit<FieldValues>
+  setFiles: (files: FileList) => void
   onPrevStep: () => void
   onNextStep: () => void
 }
 
-function AddSecondForm({ control, errors, handleSubmit, setFiles, onPrevStep, onNextStep }: Props) {
+function AddSecondForm({
+  control,
+  errors,
+  handleSubmit,
+  setFiles,
+  onPrevStep,
+  onNextStep
+}: Props) {
   const [startDate, setStartDate] = useState(new Date())
 
   return (
-    <form onSubmit={handleSubmit(onNextStep)} className="w-full flex flex-col gap-4">
+    <form
+      onSubmit={handleSubmit(onNextStep)}
+      className="w-full flex flex-col gap-4"
+    >
       {/* Add images */}
       <UploadImages setFiles={setFiles} />
 
       <article className="grid grid-cols-1 gap-4">
-        <FormTextArea
-          register={control.register}
-          rules={{ required: true }}
-          errors={errors}
-          name="whatWillYouDo"
+        <Textarea
+          id="whatWillYouDo"
           label="Qué harás"
-        />
-        <MyFormInput
           register={control.register}
-          rules={{ required: true }}
+          required={true}
           errors={errors}
-          name="goodChoice"
+        />
+        <Input
+          id="goodChoice"
           label="Buena elección para"
-        />
-        <MyFormInput
           register={control.register}
-          rules={{ required: true }}
+          required={true}
           errors={errors}
-          name="cancellationPolicy"
+        />
+        <Input
+          id="cancellationPolicy"
           label="Pólitica de cancelación"
+          register={control.register}
+          required={true}
+          errors={errors}
         />
       </article>
 
       <article className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <MyFormInput
-          register={control.register}
-          rules={{ required: true }}
-          errors={errors}
-          name="pricePerPerson"
+        <Input
+          id="pricePerPerson"
           label="Precio por persona"
-        />
-        <MyFormInput
           register={control.register}
-          rules={{ required: true }}
+          required={true}
           errors={errors}
-          name="schedule"
+        />
+        <Input
+          id="schedule"
           label="Horario"
+          register={control.register}
+          required={true}
+          errors={errors}
         />
 
         <div className="flex flex-col justify-start items-start sm:col-span-2">
@@ -70,16 +86,12 @@ function AddSecondForm({ control, errors, handleSubmit, setFiles, onPrevStep, on
           <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
             <DatePicker
               selected={startDate}
-              onChange={date => {
-                setStartDate(date)
-              }}
+              onChange={(date: Date) => setStartDate(date)}
               className="w-full border border-gray-400 text-gray-600 rounded-xl px-4 py-2 focus:outline-none focus:border-blue focus:text-blue"
             />
             <DatePicker
               selected={startDate}
-              onChange={date => {
-                setStartDate(date)
-              }}
+              onChange={(date: Date) => setStartDate(date)}
               className="w-full border border-gray-400 text-gray-600 rounded-xl px-4 py-2 focus:outline-none focus:border-blue focus:text-blue"
             />
           </div>
@@ -93,7 +105,7 @@ function AddSecondForm({ control, errors, handleSubmit, setFiles, onPrevStep, on
           background="bg-orange"
           showIcon={true}
           position="left"
-          icon={<HiOutlineArrowSmLeft size={18} />}
+          icon={HiOutlineArrowSmLeft}
         />
 
         <Button
@@ -101,7 +113,7 @@ function AddSecondForm({ control, errors, handleSubmit, setFiles, onPrevStep, on
           onClick={onNextStep}
           background="bg-orange"
           showIcon={true}
-          icon={<HiOutlineArrowSmRight size={18} />}
+          icon={HiOutlineArrowSmRight}
         />
       </article>
     </form>
