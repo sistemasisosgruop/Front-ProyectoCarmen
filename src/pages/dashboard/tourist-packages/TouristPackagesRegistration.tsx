3711cf873ff8374a0f6ce27654451d0a9097ce03
yoelvp <yoelvp73@gmail.com>
2023-06-21 19:02:47 -0500

    mover la tabla a un componente

--- src/pages/dashboard/tourist-packages/TouristPackagesRegistration.tsx
+++ src/pages/dashboard/tourist-packages/TouristPackagesRegistration.tsx
@@ -2,12 +2,11 @@ import { useModal } from '@hooks/useModal'
 import Heading from '@components/Heading'
 import AdminLayout from '@layouts/AdminLayout'
 import AdminSection from '@layouts/AdminSection'
-import THead from '@components/THead'
 import Modal from '@components/Modal'
 import AddTourPackage from '@features/tours/components/AddTourPackage'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { BiChevronDown, BiEdit, BiTrash } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
+import Tours from '@features/tours/components/Tours'
 
 function TouristPackagesResgistration() {
   const { isOpenModal, openModal, closeModal } = useModal()
@@ -41,67 +40,7 @@ function TouristPackagesResgistration() {
             </button>
           </article>
 
-          <div className="overflow-x-scroll rounded-xl">
-            <table className="w-full">
-              <THead
-                headers={[
-                  '#',
-                  'Nombre del tour',
-                  'Descripción del tour',
-                  'Extras',
-                  'Ubicación',
-                  'Duración',
-                  'Dificultad',
-                  'Idiomas',
-                  'N° personas',
-                  'Edades',
-                  'Información',
-                  'Detalles',
-                  'Acciones'
-                ]}
-                rowCellStyles="bg-blue text-white px-2 py-2 whitespace-nowrap"
-              />
-              <tbody>
-                {[...Array(5)].map((_, index) => (
-                  <tr key={index} className="text-sm">
-                    <td>{index + 1}</td>
-                    <td className="whitespace-nowrap px-4 py-2">Valle sagrado de los incas</td>
-                    <td className="px-4 py-2">
-                      El servicio blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla
-                      blabla
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-2">-</td>
-                    <td className="whitespace-nowrap px-4 py-2">Valle sagrado</td>
-                    <td className="whitespace-nowrap px-4 py-2">8 Horas</td>
-                    <td className="whitespace-nowrap px-4 py-2">Baja</td>
-                    <td className="whitespace-nowrap px-4 py-2">Español, Inglés y Portugués</td>
-                    <td className="px-4 py-2">Máximo 15 personas por grupo</td>
-                    <td className="whitespace-nowrap px-4 py-2">1 a 75 años</td>
-                    <td className="whitespace-nowrap px-4 py-2">
-                      <button type="button" className="p-1 text-dark">
-                        <BiChevronDown size={24} />
-                      </button>
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-2">
-                      <button type="button" className="p-1 text-dark">
-                        <BiChevronDown size={24} />
-                      </button>
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-2">
-                      <article className="flex justify-center items-center gap-2">
-                        <button type="button" className="p-1 text-dark">
-                          <BiEdit size={18} />
-                        </button>
-                        <button type="button" className="p-1 text-dark">
-                          <BiTrash size={18} />
-                        </button>
-                      </article>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <Tours />
         </AdminSection>
       </AdminLayout>
 
