526f3613e1312986d03e51b9448d99e5ccedb67e
yoelvp <yoelvp73@gmail.com>
2023-06-28 15:14:01 -0500

    cambio de datos por los que están en el local storage

--- src/components/shopping-cart/Product.tsx
+++ src/components/shopping-cart/Product.tsx
@@ -1,23 +1,31 @@
-function Product() {
+import { Department } from 'types/department'
+
+interface Props {
+  product: Department
+}
+
+function Product({ product }: Props) {
   return (
     <article className="flex justify-between items-center gap-4">
       <input type="checkbox" name="" />
       <div className="w-full sm:flex sm:justify-start">
         <img
-          src="https://i0.wp.com/robbreport.mx/wp-content/uploads/2019/04/tribeca-bed41-1600x995-1.jpg"
+          src={product?.roomImages?.imageUrl}
           alt="imagen producto"
           className="w-full h-32 rounded-xl object-cover object-center sm:w-48 sm:h-24"
         />
         <div className="w-full p-2 sm:px-4">
           <article className="flex justify-between items-center gap-4">
             <div>
-              <h3 className="text-dark text-lg font-bold">Habitación doble</h3>
+              <h3 className="text-dark text-lg font-bold">{product.roomType}</h3>
               <p className="text-base text-gray-600">10 diciembre - 14 diciembre</p>
             </div>
 
             <p className="flex flex-col">
-              <span className="text-gray-600 text-center text-xs">$100 / 4 días</span>
-              <span className="text-xl text-blue text-center font-bold whitespace-nowrap">$ 500.00</span>
+              {/* <span className="text-gray-600 text-center text-xs">S/. {product.price?.toLocaleString(undefined, { minimumFractionDigits: 2 })} / 4 días</span> */}
+              <span className="text-xl text-blue text-center font-bold whitespace-nowrap">
+                S/. {product.price?.toLocaleString(undefined, { minimumFractionDigits: 2 })}
+              </span>
             </p>
           </article>
 
--- src/components/shopping-cart/Products.tsx
+++ src/components/shopping-cart/Products.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
 import Product from './Product'
+import { Department } from 'types/department'
 
 function Products() {
+  const [productsInCart, setProductsInCart] = useState<Department[]>([])
+
+  useEffect(() => {
+    getDepartmentsInCart()
+  }, [])
+
+  const getDepartmentsInCart = async () => {
+    try {
+      const departments = window.localStorage.getItem('departmentCart') ?? ''
+      const departmentsJson = await JSON.parse(departments)
+      setProductsInCart(departmentsJson)
+    } catch (error) {
+      throw new Error('Dont products in cart')
+    }
+  }
+
   return (
     <div className="w-full flex flex-col justify-between gap-4">
-      {[...Array(2)].map((_, index) => (
-        <div key={index}>
-          <Product />
+      {productsInCart.map((product) => (
+        <div key={product.id}>
+          <Product product={product} />
           <hr className="border-none bg-gray-200 w-full py-[0.5px]" />
         </div>
       ))}
