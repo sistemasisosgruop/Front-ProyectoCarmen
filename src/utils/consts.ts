1629edcc51fdafcbf382ebc57abf71974cc16916
yoelvp <yoelvp73@gmail.com>
2023-06-28 15:18:23 -0500

    keys para los datos que se almacenan en el storage del cliente

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,9 @@
 export const CLIENT_ID = '522684700922-9h73sleepj9nrgu3h9f1ukgcm6katksr.apps.googleusercontent.com'
 export const SECRET_CLIENT_ID = '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'
 export const API_URL = 'http://localhost:5000/api/v1'
+
+export const keyStorage = {
+  JWT_TOKEN: 'jwtToken',
+  DEPARTMENTS_CART: 'departmentsCart',
+  TOURS_CART: 'toursCart'
+}
