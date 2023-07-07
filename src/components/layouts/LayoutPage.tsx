0df52980d1fff9bbafa8a9524e00638d9b34d3ec
yoelvp <yoelvp73@gmail.com>
2023-07-07 10:43:36 -0500

    add typed

--- src/components/layouts/LayoutPage.tsx
+++ src/components/layouts/LayoutPage.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from 'react'
+import { type ReactNode, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 import Footer from '@components/footer/Footer'
